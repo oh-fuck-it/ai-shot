4b416bf46e50336a73e2c115dd7ae8585d92f8b9
holk <yellowmac@sina.com>
2021-09-18 20:41:57 +0800

    update algorithm

--- algorithm/pose_estimate.py
+++ algorithm/pose_estimate.py
@@ -4,9 +4,10 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-# root_path = 'C:\\Users\\holk\\Documents\\Tencent Files\\1599840925\\FileRecv\\File'
-root_path = 'C:\\Users\\holk\\Documents\\Tencent Files\\1599840925\\FileRecv\\file1'
+root_path = 'C:\\Users\\holk\\Documents\\Tencent Files\\1599840925\\FileRecv\\File'
+# root_path = 'C:\\Users\\holk\\Documents\\Tencent Files\\1599840925\\FileRecv\\file1'
 image_path = root_path
+threshold = 0.5
 joints = ['nose', 'l-eye', 'r-eye', 'left ear', 'right ear', 'left shoulder', 'right shoulder', 'left elbow',
           'right elbow', 'left wrist', 'right wrist', 'left hip', 'right hip', 'left knee', 'right knee',
           'left ankle',
@@ -16,33 +17,30 @@ filename = 'result.json'
 model = hub.load("movenet_singlepose_thunder")
 
 
-def estimate(image_path, threshold=0.3):
+def estimate(image_path):
     image = tf.io.read_file(image_path)
     image = tf.compat.v1.image.decode_jpeg(image)
     image = tf.cast(tf.image.resize_with_pad(image, 256, 256), dtype=tf.int32)
-    # plt.imshow(image)
     image = tf.expand_dims(image, axis=0)
     image = tf.cast(tf.image.resize_with_pad(image, 256, 256), dtype=tf.int32)
     movenet = model.signatures['serving_default']
     outputs = movenet(image)
-    keypoints = outputs['output_0'].numpy()[0][0]
+    key_points = outputs['output_0'].numpy()[0][0]
     base_name = os.path.basename(image_path)
     joints_dict[base_name] = []
     # joints_dict[base_name].append([])
-    for i, keypoint in enumerate(keypoints):
-        # if keypoint[2] > threshold:
+    for i, keypoint in enumerate(key_points):
         joints_dict[base_name].append(keypoint.tolist())
-        # joints_dict[base_name][0].append(i)
+    if tf.reduce_sum(tf.convert_to_tensor(joints_dict[base_name])[:, 2]) < threshold:
+        del joints_dict[base_name]
 
 
-for filepath, dirnames, filenames in os.walk(root_path):
+for filepath, dir_names, filenames in os.walk(root_path):
     for image_path in filenames:
         if image_path != '.DS_Store':
             path = os.path.join(filepath, image_path)
             print("now get %s", path)
             estimate(path)
-            print(joints_dict)
 
 with open(filename, 'w') as file_obj:
     json.dump(joints_dict, file_obj)
-# plt.show()
--- algorithm/pose_similarity.py
+++ algorithm/pose_similarity.py
@@ -4,23 +4,9 @@ import json
 from tensorflow.python.ops.numpy_ops import np_config
 
 np_config.enable_numpy_behavior()
-
 filename = 'result.json'
 with open(filename) as file_obj:
     pics = json.load(file_obj)
-
-
-# def handle_joints(pic_name, threshold=0.3):
-#     coordinates = []
-#     pic = pics[pic_name]
-#     for i, joint in enumerate(pic):
-#         if i == 0:
-#             coordinates.append(joint[:])
-#             continue
-#         if joint[2] < threshold:
-#             joint[:2] = [0, 0]
-#         coordinates.append(joint[:2])
-#     return coordinates
 
 
 def cosine_similarity(q, a):
@@ -33,13 +19,41 @@ def cosine_similarity(q, a):
     return score
 
 
-def estimate_similarity(pic1_name, pic2_name):
+def estimate_two_pic_similarity(pic1_name, pic2_name):
     if pic1_name not in pics and pic2_name not in pics:
         raise Exception('invalid pic name')
-    coordinate = tf.convert_to_tensor(pics[pic1_name])[:, :2]
-    score = tf.convert_to_tensor(pics[pic1_name])[:, 2:]
-    print(tf.reshape(coordinate, [34, 1]))
-    print(tf.reduce_mean(cosine_similarity(tf.reshape(coordinate, [34, 1]),tf.reshape(coordinate, [34, 1]))))
+    coordinate1 = tf.convert_to_tensor(pics[pic1_name])[:, :2]
+    coordinate2 = tf.convert_to_tensor(pics[pic2_name])[:, :2]
+    score = tf.convert_to_tensor(pics[pic1_name])[:, 2]
+    pic1_reshape = tf.reshape(coordinate1, [1, 34])
+    pic2_reshape = tf.reshape(coordinate2, [1, 34])
+    # print(pic2_reshape, pic1_reshape)
+    return tf.reduce_mean(cosine_similarity(pic1_reshape, pic2_reshape))
 
-# print(handle_joints('0a2f7da2-1550-11ec-9b51-64bc580330d5.png', 0.3))
-print(estimate_similarity('0a256522-1550-11ec-ab0e-64bc580330d5.png', '0a12508c-1550-11ec-8261-64bc580330d5.png'))
+
+def estimate_similarity_in_all_data(pic_name, data=pics):
+    res = [0, 0]
+    data = tf.convert_to_tensor(0)
+
+    coordinate = tf.convert_to_tensor(pics[pic_name])[:, :2]
+    score = tf.convert_to_tensor(pics[pic_name])[:, 2]
+    pic_reshape = tf.reshape(coordinate, [1, 34])
+
+    for pic in pics:
+        if pic == pic_name:
+            continue
+        data_coordinate = tf.convert_to_tensor(pics[pic])[:, :2]
+        data_reshape = tf.reshape(data_coordinate, [1, 34])
+        # print(cosine_similarity(pic_reshape, data_reshape).numpy()[0])
+        similarity = cosine_similarity(pic_reshape, data_reshape)
+        tf.reshape(similarity, [])
+        # print(data)
+        if similarity > res[1]:
+            res[0] = pic
+            res[1] = similarity
+            data = data_reshape
+    return res
+
+
+print(estimate_similarity_in_all_data('02ada987-1550-11ec-8457-64bc580330d5.png'))
+# print(estimate_two_pic_similarity('02ada987-1550-11ec-8457-64bc580330d5.png','02ada987-1550-11ec-8457-64bc580330d5.png').)
