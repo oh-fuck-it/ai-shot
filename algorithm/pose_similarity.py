c4fb918c6cf91538c6a47d93af7d83589df14941
holk <yellowmac@sina.com>
2021-09-19 20:08:17 +0800

    Update pose_similarity.py

--- algorithm/pose_similarity.py
+++ algorithm/pose_similarity.py
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
+=======
+import time
+
+import tensorflow as tf
+import numpy as np
+>>>>>>> Stashed changes
 import json
 
 import tensorflow as tf
@@ -8,6 +15,8 @@ filename = '../result.json'
 with open(filename) as file_obj:
     print(file_obj)
     pics = json.load(file_obj)
+for p in pics:
+    pics[p] = tf.reshape(tf.convert_to_tensor(pics[p])[:, :2], [1, 34])
 
 def cosine_similarity(q, a):
     q = tf.convert_to_tensor(q)
@@ -27,7 +36,6 @@ def estimate_two_pic_similarity(pic1_name, pic2_name):
     score = tf.convert_to_tensor(pics[pic1_name])[:, 2]
     pic1_reshape = tf.reshape(coordinate1, [1, 34])
     pic2_reshape = tf.reshape(coordinate2, [1, 34])
-    # print(pic2_reshape, pic1_reshape)
     return [tf.reduce_mean(cosine_similarity(pic1_reshape, pic2_reshape)), score]
 
 
@@ -35,16 +43,18 @@ def estimate_similarity_in_all_data(coordinate, dataset=pics):
     res = [0, 0]
     pic_reshape = tf.reshape(tf.convert_to_tensor(coordinate)[:, :2], [1, 34])
     for pic in dataset:
-        data_coordinate = tf.convert_to_tensor(dataset[pic])[:, :2]
-        data_reshape = tf.reshape(data_coordinate, [1, 34])
-        similarity = cosine_similarity(pic_reshape, data_reshape)
+        data_coordinate = dataset[pic]
+        similarity = cosine_similarity(pic_reshape, data_coordinate)
         tf.reshape(similarity, [])
         if similarity > res[1]:
             res[0] = pic
             res[1] = similarity
     return res
+<<<<<<< Updated upstream
 
 #
 # print(pics['cuStP_i-xPg.png'])
 # print(estimate_similarity_in_all_data(pics['cuStP_i-xPg.png']))
 # print(estimate_two_pic_similarity('02ada987-1550-11ec-8457-64bc580330d5.png','02ada987-1550-11ec-8457-64bc580330d5.png').)
+=======
+>>>>>>> Stashed changes
